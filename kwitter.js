a8de3652b61a27d819746aab87361309f320e19c
Tarashrita <106442772+Tarashrita@users.noreply.github.com>
2022-12-24 23:23:12 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,15 @@
 function addUser() {
-    user_name = document.getElementById("user_name").value;
-    localStorage.setItem("user_name", user_name);
-    window.location = "kwitter_room.html";
+  user_name = document.getElementById("user_name").value;
+  localStorage.setItem("user_name", user_name);
+  window.location = "kwitter_room.html";
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function AddUser() {
+
+  user_name=document.getElementById("user_name").value;
+  firebase.database().ref("/").child(user_name).update({
+    purpose:"adding user"});
 }
